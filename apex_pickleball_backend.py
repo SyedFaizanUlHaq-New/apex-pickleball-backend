c1546adae13a19fd24ef9d0eb07e2d77281b3920
Syed <87998470+SyedFaizanUlHaq-New@users.noreply.github.com>
2026-04-25 04:07:33 +0500

    Add files via upload

--- apex_pickleball_backend.py
+++ apex_pickleball_backend.py
@@ -12,12 +12,8 @@ app = FastAPI(title="APEX Pickleball Generator")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "https://matchs.apexpickle.ca",
-    "http://matchs.apexpickle.ca",
-],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
